b0952e23627212261d257e063fac9a7ae5440655
ancrila27 <ac.landicho@hotmail.com>
2018-03-11 19:02:37 +0000

    separated classes, addition of graphing and sample csv file

--- LightSensor.py
+++ LightSensor.py
@@ -3,6 +3,7 @@ import numpy as np
 from picamera.array import PiRGBAnalysis
 from picamera.color import Color
 import math
+from datetime import datetime
 
 class LightSensor(PiRGBAnalysis):
     def __init__(self, camera):
@@ -11,17 +12,13 @@ class LightSensor(PiRGBAnalysis):
 
 
     def analyze(self, a):
+       
+        #c  = Color( a )
         # Convert the average color of pixels in the middle box
         c = Color(
             r=int(np.mean(a[30:60, 60:120, 0])),
             g=int(np.mean(a[30:60, 60:120, 1])),
             b=int(np.mean(a[30:60, 60:120, 2]))
-            )
-
-        brightness = math.sqrt(
-            (c.red**2) * 0.241 +
-            (c.green**2) * 0.691 +
-            (c.blue**2) * 0.068
             )
         
         # Convert the color to hue, saturation, lightness
@@ -35,25 +32,28 @@ class LightSensor(PiRGBAnalysis):
             c_status = 'OFF'
 
         if c_status != self.status:
+            print(a.shape)
             self.status = c_status
-            print(c_status)
-        
+            print(str(datetime.now()) + " (" + str(c_status) + ") L:" + str(l))
 
 
-with picamera.PiCamera(resolution='160x90', framerate=30) as camera:
-    # fix camera's white-balance gains
+with picamera.PiCamera(resolution='64x64', framerate=30) as camera:
     camera.awb_mode = 'off'
     camera.awb_gains = (1.4, 1.5)
-    # draw a box over the area we're going to watch
-    camera.start_preview() #alpha=128
-    box = np.zeros((96, 160, 3), dtype=np.uint8)
-    box[30:60, 60:120, :] = 0x80
-    #construct the analysis output and start recording data to it
+
+    camera.start_preview()
     with LightSensor(camera) as analyser:
         camera.start_recording(analyser, 'rgb')
         try:
             while True:
-                camera.wait_recording(1)
+                camera.wait_recording(10000)
         finally:
-            camera_stop_recording()
+            camera.stop_recording()
 
+    
+  
+    # draw a box over the area we're going to watch
+##    camera.start_preview() #alpha=128
+##    box = np.zeros((96, 160, 3), dtype=np.uint8)
+##    box[30:60, 60:120, :] = 0x80
+    #construct the analysis output and start recording data to it
